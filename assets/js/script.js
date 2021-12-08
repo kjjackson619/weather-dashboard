a8c3cea9a3cb4451bb229fba3f31ea0bb0a38117
KJ Jackson <kjjackson619@yahoo.com>
2021-12-07 23:12:36 -0500

    add click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,20 @@
 var searchBtnEl = document.querySelector("#btn");
 var cityNameEl = document.querySelector("#city-name");
 
+var forecastEl = document.querySelector("#forecast");
+var weatherContainerEl = document.querySelector("#weather-container");
 
+var chicago = "https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=2cf57f3bc628ca68fe4f4198c9697a35";
 
 var getWeather = function () {
 
     //api for Chicago
-    var chicago = "https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
     fetch(chicago).then(function (response) {
         response.json().then(function (data) {
             console.log(data);
         });
     });
-
-    //api for Seattle
-    var seattle = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(seattle).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-
-    //api for Austin
-    var austin = "https://api.openweathermap.org/data/2.5/weather?q=austin&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(austin).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-
-    //api for Denver 
-    var denver = "https://api.openweathermap.org/data/2.5/weather?q=denver&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(denver).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-
-    //api for Miami
-    var miami = "https://api.openweathermap.org/data/2.5/weather?q=miami&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(miami).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-
-    //api for Orlando
-    var orlando = "https://api.openweathermap.org/data/2.5/weather?q=orlando&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(orlando).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-
-    //api for Columbus
-    var columbus = "https://api.openweathermap.org/data/2.5/weather?q=columbus&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(columbus).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-
-    //api for Atlanta
-    var atlanta = "https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(atlanta).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-
-    //api for LA
-    var losAngeles = "https://api.openweathermap.org/data/2.5/weather?q=lax&appid=2cf57f3bc628ca68fe4f4198c9697a35";
-
-    fetch(losAngeles).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
-};
+}
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -95,11 +24,34 @@ var formSubmitHandler = function (event) {
     if (city) {
         getWeather(city);
         cityNameEl.value = "";
-        console.log(city);
+
     } else {
         alert("Please enter valid city!");
     }
 };
+
+var displayWeather = function (name, main) {
+    response.json().then(function (chicago) {
+        displayWeather(chicago, name);
+    });
+
+    forecastEl.textContent = "";
+    weatherContainerEl.textContent = main;
+
+    for (var i = 0; i < chicago.length; i++) {
+        var dailyForecast = chicago[i];
+
+        var dayEl = document.createElement("div");
+        dayEl.innerText = dailyForecast;
+
+        var titleEl = document.createElement("span");
+        titleEl.innerText = displayWeather;
+
+        dayEl.appendChild(titleEl);
+
+        forecastEl.appendChild(dailyForecast);
+    }
+}
 
 searchBtnEl.addEventListener("click", formSubmitHandler);
 
