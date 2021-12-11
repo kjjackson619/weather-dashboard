c8bb21bb0589e887a8d4d8823d4011b85e0f3b4a
KJ Jackson <kjjackson619@yahoo.com>
2021-12-11 16:19:21 -0500

    functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,80 @@
-var searchBtnEl = document.querySelector("#btn");
-var cityNameEl = document.querySelector("#city-name");
+var apiKey = "2cf57f3bc628ca68fe4f4198c9697a35"
 
-var forecastEl = document.querySelector("#forecast");
-var weatherContainerEl = document.querySelector("#weather-container");
 
-var chicago = "https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=2cf57f3bc628ca68fe4f4198c9697a35";
+var searchBtn = $(".searchButton");
 
-var getWeather = function () {
+for (var i = 0; i < localStorage.length; i++) {
+    var city = localStorage.getItem(i);
 
-    //api for Chicago
-    fetch(chicago).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        });
-    });
+    var cityName = $(".list-group").addClass("list-group-item");
+
+    cityName.append("<li>" + city + "</li>");
 }
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
+var keyCount = 0;
 
-    var city = cityNameEl.value.trim();
+searchBtn.click(function () {
 
-    if (city) {
-        getWeather(city);
-        cityNameEl.value = "";
+    var searchInputEl = $(".searchInput").val();
 
+    var apiWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputEl + "&Appid=" + apiKey + "&units=imperial";
+
+    var apiForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInputEl + "&Appid=" + apiKey + "&units=imperial";
+
+    if (searchInputEl == "") {
+        console.log(searchInputEl);
     } else {
-        alert("Please enter valid city!");
+        $.ajax({
+            url: apiWeatherUrl,
+            method: "GET",
+        }).then(function (response) {
+            var cityName = $(".list-group").addClass("list-group-item");
+            cityName.append("<li>" + response.name + "</li>");
+            var local = localStorage.setItem(keyCount, response.name);
+            keyCount = keyCount + 1;
+
+            var currentCard = $(".currentCard").append("<p>");
+            currentCard.append(currentName);
+
+            var time = new Date(response.dt * 1000);
+            currentName.append(response.name + " " + time.toLocaleDateString("en-US"));
+            currentName.append('img src = "https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">');
+            var currentTemp = currentName.append("<p>");
+            currentName.append(currentTemp);
+            currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+            currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+
+
+            var apiUVUrl = 'https://api.openweathermap.org/data/2.5/uvi?appid=2cf57f3bc628ca68fe4f4198c9697a35&lat=${response.coord.lat}&lon=${response.coord.lon}';
+
+            $.ajax({
+                url: apiUVUrl,
+                method: "GET"
+            }).then(function (response) {
+
+                var currentUV = currentTemp.append("<p>" + "UV Index: " + response.value + "</p>").addClass("card-text");
+                currentUV.addClass("UV");
+                currentTemp.append(currentUV);
+            });
+        });
+
+        $.ajax({
+            url: apiForecastUrl,
+            method: "GET"
+        }).then(function (response) {
+            var day = [0, 8, 16, 24, 32];
+            var fiveDayEl = $(".fiveDayCard").addClass("card-body");
+            var fiveDays = $(".fiveDayOne").addClass("card-text");
+            fiveDays.empty();
+            day.forEach(function (i) {
+                var fiveDayTime = new Date(response.list[i].dt * 1000);
+                fiveDayTime = fiveDayTime.toLocaleDateString("en-US");
+
+                fiveDays.append("<div class=fiveDayColor>" + "<p>" + fiveDayTime + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "F" + "</p>" + "<p>" + "Wind: " + response.list[i].main.wind_speed + "MPH" + "</p>" + "<p>" + "Humidty: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+            })
+        });
     }
-};
-
-var displayWeather = function (name, main) {
-    response.json().then(function (chicago) {
-        displayWeather(chicago, name);
-    });
-
-    forecastEl.textContent = "";
-    weatherContainerEl.textContent = main;
-
-    for (var i = 0; i < chicago.length; i++) {
-        var dailyForecast = chicago[i];
-
-        var dayEl = document.createElement("div");
-        dayEl.innerText = dailyForecast;
-
-        var titleEl = document.createElement("span");
-        titleEl.innerText = displayWeather;
-
-        dayEl.appendChild(titleEl);
-
-        forecastEl.appendChild(dailyForecast);
-    }
-}
-
-searchBtnEl.addEventListener("click", formSubmitHandler);
-
-getWeather();
 
 
+
+});
